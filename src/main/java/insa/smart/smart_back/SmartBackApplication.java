a847bb304ba6933be49993648e731705badf1413
Cyril <cyril.gomes@outlook.com>
2022-04-13 08:44:00 +0200

    init

---
+++ src/main/java/insa/smart/smart_back/SmartBackApplication.java
@@ -0,0 +1,13 @@
+package insa.smart.smart_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/insa/smart/smart_back/SmartBackApplicationTests.java
@@ -0,0 +1,13 @@
+package insa.smart.smart_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
