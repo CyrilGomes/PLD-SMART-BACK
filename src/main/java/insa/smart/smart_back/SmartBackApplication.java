af7ef282c166c01cb4e70f94fd64ad3c8d94c7f4
Cyril <cyril.gomes@outlook.com>
2022-04-15 09:21:46 +0200

    testo

--- src/main/java/insa/smart/smart_back/SmartBackApplication.java
+++ src/main/java/insa/smart/smart_back/SmartBackApplication.java
@@ -2,10 +2,28 @@ package insa.smart.smart_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@SpringBootApplication
+import java.util.Map;
+
+@Controller
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SmartBackApplication {
 
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/env")
+    public @ResponseBody
+    Map<String, String> env() {
+        return System.getenv();
+    }
     public static void main(String[] args) {
         SpringApplication.run(SmartBackApplication.class, args);
     }
